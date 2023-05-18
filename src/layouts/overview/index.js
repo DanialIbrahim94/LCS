c299a90d0612f05495b34ab6ac7c9fbcc462e7fb
Araari adem <araariadem0@gmail.com>
2023-05-18 23:18:14 +0200

    quick fix for minor issue

--- src/layouts/overview/index.js
+++ src/layouts/overview/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
@@ -36,6 +36,13 @@ const StyledGuideBox = styled(Box)(({ theme }) => ({
 }));
 
 function Overview() {
+  const userinfo = JSON.parse(sessionStorage.getItem("userData"));
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userinfo || userinfo.role.id !== 2) navigate("/tables");
+  }, []);
+
   return (
     <DashboardLayout>
       <DashboardNavbar />
